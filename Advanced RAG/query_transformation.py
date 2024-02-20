90d1d85e338a8fc4ea27028336469e68e1b8a509
KevinTai0808 <kevintsai0808@gmail.com>
2024-02-21 00:22:52 +0800

    feat: add multi-step query transformation

--- Advanced RAG/query_transformation.py
+++ Advanced RAG/query_transformation.py
@@ -1,6 +1,7 @@
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.core.indices.query.query_transform.base import  HyDEQueryTransform
-from llama_index.core.query_engine import TransformQueryEngine
+from llama_index.core.indices.query.query_transform.base import  HyDEQueryTransform, StepDecomposeQueryTransform
+from llama_index.core.query_engine import TransformQueryEngine, MultiStepQueryEngine
+from llama_index.llms.openai import OpenAI
 from dotenv import load_dotenv
 import os
 
@@ -12,6 +13,10 @@ documents = SimpleDirectoryReader(input_files=["./data/example1/paul_graham_essa
 index = VectorStoreIndex.from_documents(documents)
 
 query_engine = index.as_query_engine()
+
+"""
+HyDE : Create hypothetical answer based on initial query
+"""
 query_str = "what did paul graham do after going to RISD"
 response = query_engine.query(query_str)
 print("\nBase response :\n",response)
@@ -25,10 +30,45 @@ print("\nHyDE response :\n",response)
 hypo_doc = hyde(query_str)
 print("\nHypothetical Answer : \n",hypo_doc.embedding_strs[0])
 
-"""summary
+"""
+summary
 1. if initial query is brief, it may be more appropriate to 
 use initial query instead of new query(by HyDE) to do retrieval
 
 2. for the query that is an open-ended question, 
 the hypothetical answer may have negative effect in retrieval
 """
+
+
+"""
+Multi-Step query transformation : decompose the initial query into subqueries
+"""
+
+index_summary = "Ready to search"
+step_decompose_transform = StepDecomposeQueryTransform(llm=OpenAI(model='gpt-4'), verbose=True)
+query_engine = MultiStepQueryEngine(
+    query_engine=query_engine, 
+    query_transform=step_decompose_transform,
+    index_summary=index_summary)
+# prompt = MultiStepQueryEngine.get_prompts(query_engine)
+# prompt : "The original question is as follows: {query_str}\nWe have an opportunity to answer some, 
+# or all of the question from a knowledge source. Context information for the knowledge source is provided 
+# below, as well as previous reasoning steps.\nGiven the context and previous reasoning, return a question that
+# can be answered from the context. This question can be the same as the original question, or this question can represent
+# a subcomponent of the overall question.It should not be irrelevant to the original question.\nIf we cannot extract 
+# more information from the context, provide 'None' as the answer. Some examples are given below: \n\nQuestion: 
+# How many Grand Slam titles does the winner of the 2020 Australian Open have?\nKnowledge source context: Provides 
+# names of the winners of the 2020 Australian Open\nPrevious reasoning: None\nNext question: Who was the winner of 
+# the 2020 Australian Open? \n\nQuestion: Who was the winner of the 2020 Australian Open?\nKnowledge source context: 
+# Provides names of the winners of the 2020 Australian Open\nPrevious reasoning: None.\nNew question: Who was the winner
+# of the 2020 Australian Open? \n\nQuestion: How many Grand Slam titles does the winner of the 2020 Australian Open have?
+# \nKnowledge source context: Provides information about the winners of the 2020 Australian Open\nPrevious reasoning:
+# \n- Who was the winner of the 2020 Australian Open? \n- The winner of the 2020 Australian Open was Novak Djokovic.\n
+# New question: None\n\nQuestion: How many Grand Slam titles does the winner of the 2020 Australian Open have?\nKnowledge
+# source context: Provides information about the winners of the 2020 Australian Open - includes biographical information 
+# for each winner\nPrevious reasoning:\n- Who was the winner of the 2020 Australian Open? \n- The winner of the 2020 Australian
+#  Open was Novak Djokovic.\nNew question: How many Grand Slam titles does Novak Djokovic have? \n\nQuestion: {query_str}\nKnowledge
+# source context: {context_str}\nPrevious reasoning: {prev_reasoning}\nNew question: 
+
+response = query_engine.query("Who was in the first batch of the accelerator program the author started")
+print(response)
